65b20a73ff675c62333ab50060d735b94136ac6f
ARAIVR <56075707+ARAIVR@users.noreply.github.com>
2026-01-19 06:22:52 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include "vulkan_fps.h"
+
+int main() {
+    VulkanRenderer renderer;
+
+    try {
+        renderer.initWindow();
+        renderer.initVulkan();
+        renderer.mainLoop();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    renderer.cleanup();
+    return EXIT_SUCCESS;
+}
